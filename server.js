96ec845037ffc10ca9ca56a2f2818472bf3af422
Justin M <manningjustin08@yahoo.com>
2023-08-24 14:37:31 -0400

    Fixed bugs

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const app = express();
-const fruits = require('./models/fruit.js');
+const Fruit = require('./models/fruit.js')
 const vegetables = require('./models/Vegetable.js');
-
+const mongoose = require('mongoose');
 const port = 3007;
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+mongoose.connection.once("open", () => {
+    console.log("connected to MongoDB")
+});
+
 
 //Setting up view engine
 app.set('views', __dirname + '/views');
@@ -15,14 +26,21 @@ app.use((req, res, next) =>{
      next();
 })
 
+// Middleware
+app.use((req, res, next) => {
+    console.log('I run for all routes!');
+    next();
+})
+
 
 app.use(express.urlencoded({extended:false}))
 
 
 //ROUTES
 //index
-app.get('/fruits', (req, res)=>{
-    res.render("fruits/Index", {
+app.get('/fruits', async function(req, res) {
+    const foundFruits = await Fruit.find({})
+    res.render("fruits/index", {
         fruits: fruits
     })
 });
@@ -46,6 +64,27 @@ app.get('/fruits/:indexOfFruitsArray', (req, res)=>{
     })
 })
 
+// CREATE = POST
+
+app.post("/fruits", async(req, res)=> {
+    console.log(req.body)
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    const createdFruit = await Fruit.create(req.body)
+    console.log(createdFruit)
+    res.redirect('/fruits')
+})
+
+
+app.get("/fruits/:id", async(req, res) =>{
+    const oneFruit = await fruit.findById(req.params.id)
+    res.render('fruits/Show', {
+        fruit: oneFruit
+    })
+});
 
 
 app.get('/vegetable', (req, res)=>{
@@ -60,6 +99,20 @@ app.get('/vegetable/:indexOfVegetablesArray', (req, res)=>{
     res.render('veggies/Show', {
         vegetables: vegetables[req.params.indexOfVegetablesArray]
     })
+})
+
+// CREATE = POST
+
+app.post("/vegetables", async(req, res)=> {
+    console.log(req.body)
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    const createdVegetable = await vegetables.create(req.body)
+    console.log(createdVegetable)
+    res.redirect('/vegetables')
 })
 
 
--- views/fruits/Index.jsx
+++ views/fruits/Index.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 
-function Index({fruits}) {
-  return (
-    <div>
-        {
-            fruits.map((fruit, i)=> {
+function Index({ fruits }) {
+    return (
+        <div>
+
+            <nav>
+                <a href='/fruits/new'>Create New Fruit</a>
+            </nav>
+
+            {fruits.map((fruit, i) => {
                 return (
                     <li key={i}>
-                        <a href={`/fruits/${i}`}>  {fruit.name} </a> is{fruit.color} <br /> 
+                        <a href={`/fruits/${fruit.id}`}>  {fruit.name} </a> is{fruit.color} <br />
                         {
                             fruit.readyToEat ? "it is ready to eat" : "it is not ready to eat"
                         }
                     </li>
                 )
             })
-        }
-    </div>
-  )
+            }
+        </div>
+    )
 }
+
 
 module.exports = Index
